9d61e28fd43c85a1888ab9dcb2e9af00b02bca0d
Berkay Kabay <mail@berkaykabay.de>
2019-08-14 17:13:43 +0200

    rtz

---
+++ db/migrate/20190814124302_changestarstorating.rb
@@ -0,0 +1,5 @@
+class Changestarstorating < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :reviews, :stars, :rating
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,41 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Jolly',
+    address:      'Museumsinsel',
+    phone_number: '1234567890',
+    category:     'Chinese'
+  },
+  {
+    name:         'Ishin',
+    address:      'Friedrichstr',
+    phone_number: '2345678901',
+    category:     'Japanese'
+  },
+  {
+    name:         'Leffe',
+    address:      'Belgianstr',
+    phone_number: '3456789012.',
+    category:     'Belgian'
+  },
+  {
+    name:         'Olala',
+    address:      'Libertestr',
+    phone_number: '4567890123',
+    category:     'French'
+  },
+  {
+    name:         'Luigi',
+    address:      'Romastr',
+    phone_number: '5678901234',
+    category:     'Italian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
